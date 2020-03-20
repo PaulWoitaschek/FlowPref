9790dd2bbc09f4941923102820aafa4c031fc9a3
Paul Woitaschek <woitaschek@posteo.de>
2020-03-20 21:54:30 +0100

    Implemented a memory pref.

---
+++ memory/src/main/java/de/paulwoitaschek/flowpref/memory/MemoryPref.kt
@@ -0,0 +1,35 @@
+package de.paulwoitaschek.flowpref.memory
+
+import de.paulwoitaschek.flowpref.Pref
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.channels.ConflatedBroadcastChannel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlin.reflect.KProperty
+
+
+@Suppress("unused")
+@OptIn(ExperimentalCoroutinesApi::class, FlowPreview::class)
+class MemoryPref<T>(private val default: T) : Pref<T>() {
+
+  private val channel = ConflatedBroadcastChannel(default)
+
+  override fun setAndCommit(value: T) {
+    channel.offer(value)
+  }
+
+  override val flow: Flow<T> = channel.asFlow()
+
+  override fun delete() {
+    channel.offer(default)
+  }
+
+  override fun getValue(thisRef: Any, property: KProperty<*>): T {
+    return channel.value
+  }
+
+  override fun setValue(thisRef: Any, property: KProperty<*>, value: T) {
+    channel.offer(value)
+  }
+}
