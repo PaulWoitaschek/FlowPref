49489744915937596e67d6f044008e3bbb27c873
Paul Woitaschek <woitaschek@posteo.de>
2020-03-21 21:55:54 +0100

    Updated the pref tests to check the latest value as well as the flow collection results.

--- android/src/test/java/de/paulwoitaschek/flowpref/android/FlowPrefTest.kt
+++ android/src/test/java/de/paulwoitaschek/flowpref/android/FlowPrefTest.kt
@@ -3,12 +3,6 @@ package de.paulwoitaschek.flowpref.android
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.google.common.truth.Truth.assertThat
-import de.paulwoitaschek.flowpref.android.internal.adapter.IntAdapter
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -26,101 +20,61 @@ class FlowPrefTest {
 
   @Test
   fun flowUpdated() {
-    val pref = prefs.int("key", 42)
-    val values = mutableListOf<Int>()
-    val collectJob = GlobalScope.launch(Dispatchers.Main) {
-      pref.flow.collect {
-        values += it
-      }
+    PrefTester(prefs.int("key", 42)).run {
+      assert(42)
+      put(43)
+      assert(42, 43)
+      put(42)
+      assert(42, 43, 42)
+      release()
     }
-
-    fun assert(vararg expected: Int) {
-      assertThat(values).isEqualTo(expected.toList())
-    }
-
-    fun put(value: Int) {
-      pref.setAndCommit(value)
-    }
-
-    assert(42)
-    put(43)
-    assert(42, 43)
-    put(42)
-    assert(42, 43, 42)
-
-    collectJob.cancel()
   }
 
   @Test
   fun onlyChangedFlowTriggers() {
-    val prefA = prefs.int("preA", 42)
-    val valuesA = mutableListOf<Int>()
-    val collectJobA = GlobalScope.launch(Dispatchers.Main) {
-      prefA.flow.collect {
-        valuesA += it
-      }
-    }
+    val testerA =
+      PrefTester(prefs.int("preA", 42))
+    val testerB =
+      PrefTester(prefs.int("prefB", 42))
 
-    val prefB = prefs.create("prefB", 42, IntAdapter)
-    val valuesB = mutableListOf<Int>()
-    val collectJobB = GlobalScope.launch(Dispatchers.Main) {
-      prefB.flow.collect {
-        valuesB += it
-      }
-    }
 
     fun assert(a: List<Int>, b: List<Int>) {
-      assertThat(valuesA).isEqualTo(a)
-      assertThat(valuesB).isEqualTo(b)
+      testerA.assert(*a.toTypedArray())
+      testerB.assert(*b.toTypedArray())
     }
 
-    prefA.setAndCommit(1)
+    testerA.put(1)
     assert(
       a = listOf(42, 1),
       b = listOf(42)
     )
 
-    prefA.setAndCommit(2)
+    testerA.put(2)
     assert(
       a = listOf(42, 1, 2),
       b = listOf(42)
     )
 
-    prefB.setAndCommit(1)
+    testerB.put(1)
     assert(
       a = listOf(42, 1, 2),
       b = listOf(42, 1)
     )
 
-    collectJobA.cancel()
-    collectJobB.cancel()
+    testerA.release()
+    testerB.release()
   }
 
   @Test
   fun delete() {
-    val pref = prefs.int("key", 42)
-    val values = mutableListOf<Int>()
-    val collectJob = GlobalScope.launch(Dispatchers.Main) {
-      pref.flow.collect {
-        values += it
-      }
+    PrefTester(prefs.int("key", 42)).run {
+      assert(42)
+      put(43)
+      assert(42, 43)
+      delete()
+      assert(42, 43, 42)
+      release()
     }
-
-    fun assert(vararg expected: Int) {
-      assertThat(values).isEqualTo(expected.toList())
-    }
-
-    fun put(value: Int) {
-      pref.setAndCommit(value)
-    }
-
-    assert(42)
-    put(43)
-    assert(42, 43)
-    pref.delete()
-    assert(42, 43, 42)
-
-    collectJob.cancel()
   }
 
   @Test
@@ -141,12 +95,15 @@ class FlowPrefTest {
       }
     }
     val personPref = prefs.create("key", null, personAdapter)
-    assertThat(personPref.value).isNull()
-    val peter = Person("Peter")
-    personPref.value = peter
-    assertThat(personPref.value).isEqualTo(peter)
-    personPref.value = null
-    @Suppress("USELESS_CAST") // compiler bug?
-    assertThat(personPref.value as Person?).isNull()
+
+    PrefTester(personPref).run {
+      val peter = Person("Peter")
+      assert(null)
+      put(peter)
+      assert(null, peter)
+      put(null)
+      assert(null, peter, null)
+      release()
+    }
   }
 }
